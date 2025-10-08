586fe9dc084db9d987afca24960322b7c7c628e4
Erfandev88 <erfangaaan@gmail.com>
2025-10-08 19:50:54 +0330

    Update config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS', ['keyA', 'keyB']),
+    keys: env.array("APP_KEYS", ["keyA", "keyB"]),
   },
+  url: env("PUBLIC_URL", "https://chat-ai-uf5u.onrender.com"),
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['*'],
+    },
+  },
+  allowedHosts: env.array('ALLOWED_HOSTS', ['chat-ai-uf5u.onrender.com']),
 });
