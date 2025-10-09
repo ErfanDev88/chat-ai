546143b1efcc065ce0efc2b6afa24bc3071a81d5
Erfandev88 <erfangaaan@gmail.com>
2025-10-09 20:49:53 +0330

    fix: allow unsafe-eval for Strapi admin

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -9,4 +9,19 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"], 
+          'connect-src': ["'self'", 'https:', 'data:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'frame-src': ["'self'", 'https:'],
+        },
+      },
+    },
+  },
 ];
