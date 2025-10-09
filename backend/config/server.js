72f617fd82cc3394035b33e9fe40e5c14dc9fc18
Erfandev88 <erfangaaan@gmail.com>
2025-10-09 21:19:31 +0330

    Update strapi configs

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,26 +1,13 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-
+  url: env("PUBLIC_URL", "https://chat-ai-uf5u.onrender.com"),
   app: {
     keys: env.array("APP_KEYS", ["keyA", "keyB"]),
   },
-
-  url: env("PUBLIC_URL", "https://chat-ai-uf5u.onrender.com"),
-
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-
-  settings: {
-    cors: {
-      enabled: true,
-      origin: ['*'], 
-    },
-  },
-
   allowedHosts: env.array("ALLOWED_HOSTS", [
     "chat-ai-uf5u.onrender.com",
+    "https://chat-ai-limited.vercel.app",
     "localhost",
   ]),
 });
